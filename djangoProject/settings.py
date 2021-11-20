adee7983781cf0334ab1ce7b7b9a03cab8b7b7f3
vraghavi <vannalaraghavi@gmail.com>
2021-11-19 20:32:22 -0600

    database update

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +80,9 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {'default':dj_database_url.config()}
 
 DATABASES = {
     'default': {
