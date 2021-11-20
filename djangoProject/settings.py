49a4ef85ce8d7e5f10d70fd1c8e88bdd804e8b90
vraghavi <vannalaraghavi@gmail.com>
2021-11-19 20:08:41 -0600

    whitenoise test

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -137,6 +137,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT_URL = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
